cbab2fcd1d93867856925ac8738260ed2d8a186a
Frael <frae1984@gmail.com>
2020-09-20 23:57:47 +0100

    Verificacion de la conexion

--- Proyecto - Fruteria La Madurita/src/Project/Conexion.java
+++ Proyecto - Fruteria La Madurita/src/Project/Conexion.java
@@ -18,8 +18,10 @@ public class Conexion {
         
         Class.forName("oracle.jdbc.driver.OracleDriver");
         conexion = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE", "CAMPOS_FF", "1234");
-        
+        if(conexion != null)
         return conexion;
+        else
+            return null;
     }
     
     public static Statement getStatement() throws ClassNotFoundException, SQLException{
@@ -34,5 +36,10 @@ public class Conexion {
         result = getStatement().executeQuery(sql);
         
         return result;
+    }
+    
+    public static void main(String a[]) throws ClassNotFoundException, SQLException{
+    
+        getConnection();
     }
 }
